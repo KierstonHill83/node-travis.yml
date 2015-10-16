5e2ffc366b9186c0a93c443d0fbc68bc3755bff0
KierstonHill83 <khill1218@gmail.com>
2015-10-16 10:35:23 -0600

    Initial commit

---
+++ test/test-server.js
@@ -0,0 +1,28 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var mongoose = require('mongoose');
+
+var server = require('../server/app');
+
+var should = chai.should();
+chai.use(chaiHttp);
+
+
+
+it('should return a 200 response', function(done) {
+  chai.request(server)
+  .get('/')
+  .end(function(res) {
+    res.should.have.status(200);
+    done();
+  });
+});
+
+it('should return a 404 response', function(done) {
+  chai.request(server)
+  .get('/')
+  .end(function(res) {
+    res.should.have.status(404);
+      done();
+  });
+});
